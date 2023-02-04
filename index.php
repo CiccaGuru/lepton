5b8da0662befbbab4903cdb558ddb40560989615
Roberto Ciccarelli <robyciccarelli.rc@gmail.com>
2023-02-04 19:32:50 +0100

    initial

--- index.php
+++
@@ -1,9 +0,0 @@
-<!doctype html>
-<html>
-  <head>
-    <title>VOLTAIRE - AISF Voting System</title>
-  </head>
-  <body>
-  Voting On Line Technology And Interactive Results Engine
-  </body>
-  <html>
---
+++ src/Base/Application.php
@@ -0,0 +1,13 @@
+<?php
+namespace Lepton;
+
+class Application{
+  function __construct(){
+    return;
+  }
+
+  function run(){
+    return "ciao";
+  }
+}
+?>
